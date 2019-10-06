2bd14b87dab543fe4e07efcdce090e5a4df16b01
amr elzeky <amr.elzeky@gmail.com>
2019-10-06 23:15:13 +0200

    add new post

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,4 +2,12 @@
 
 
 class Post extends AppModel {
+    public $validate = array(
+        'title' => array(
+            'rule' => 'notBlank'
+        ),
+        'body' => array(
+            'rule' => 'notBlank'
+        )
+    );
 }
